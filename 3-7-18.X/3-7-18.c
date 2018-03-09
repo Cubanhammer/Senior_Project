0bfe97647359c61a0276647c886c3e2152c67314
Unknown <hraelcortes@gmail.com>
2018-03-08 20:15:22 -0500

    modular functions and cleanup

--- 3-7-18.X/3-7-18.c
+++ 3-7-18.X/3-7-18.c
@@ -1,18 +1,5 @@
 
 
-
-/*
- * The following sample application will create a PWM to buzzer and make
- * a frequency appear at the output. Timer2 is used with the PWM module.
- *
- * By R. Most
- *
- * V1.10
- *
- * 2/22/2008
- *
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <p18f4520.h>
@@ -50,8 +37,10 @@ int freqravg;
 int freqlavg;
 int timercount;
 int errorfreq;
-
-
+int x1;
+int x2;
+int y1;
+int y2;
 
 
 void enc_check (void);
@@ -98,8 +87,8 @@ void frequencycalc (void)
    
     encodercountr2 = 0;
     encodercountl2 = 0;
-TMR0H = 0x3C;
-TMR0L = 0xB0;
+    TMR0H = 0x3C;
+    TMR0L = 0xB0;
     INTCONbits.TMR0IF = 0;
 }
 void enc_check (void) 
@@ -123,11 +112,82 @@ void enc_check (void)
             }
     }
 
-int x1;
-int x2;
-int y1;
-int y2;
+void inoutinit (void)
+{
+    TRISB = 0;
+            
+    TRISBbits.TRISB2 = 1;   //R encoder input
+    TRISBbits.TRISB1 = 1;   //L encoder input
+    TRISBbits.TRISB0 = 1;
+    
+    TRISCbits.TRISC1 = 0;	// R signal out
+    TRISCbits.TRISC2 = 0;   // L signal out
+    TRISAbits.TRISA4 = 1;
+}
+void timerinit (void)
+{
+    T0CONbits.T08BIT = 0;
+    T0CONbits.T0CS = 0;
+    T0CONbits.PSA = 1;
+    TMR0H = 0x3C;
+    TMR0L = 0xB0;
+    T0CONbits.TMR0ON = 1;
+}
 
+void intinit (void)
+{
+    RCONbits.IPEN   = 1; // enable interrupt priority
+    INTCONbits.GIE  = 1; // enable interrupts
+    INTCONbits.PEIE = 1; // enable peripheral interrupts.
+    INTCON3bits.INT1IE = 1;
+    INTCON3bits.INT2IE = 1;
+    INTCON3bits.INT1P = 0;
+    INTCON3bits.INT2P = 0;
+    INTCON2bits.INTEDG1 = 0;
+    INTCON2bits.INTEDG2 = 0;
+    
+    INTCONbits.TMR0IE = 1;
+    INTCON2bits.TMR0IP = 0;
+}
+
+void speedupdate(void)
+{
+    PORTCbits.RC2 = 1;
+    Delay10TCYx(x1);
+    PORTCbits.RC2 = 0;
+    Delay10TCYx(y1);
+    PORTCbits.RC1 = 1;
+    Delay10TCYx(x2);
+    PORTCbits.RC1 = 0;
+    Delay10TCYx(y2);
+}
+void calcerror (void)
+{
+    x2 = slavePower;
+    errorprev = error;
+    error = encodercountl - encodercountr;
+    errorfreq = freqlavg - freqravg;
+     
+    
+    derror = error - errorprev;
+ 
+    slavePower += ((errorfreq * kp) + (derror * kd) + (ierror * ki));
+    
+    if( slavePower >= 156)
+    {
+        slavePower = 150;
+    }
+    if(slavePower <= 103)
+    {
+        slavePower = 150;
+    }
+    
+    ierror = ierror + error;
+    
+   
+    encodercountl = 0;
+    encodercountr = 0;
+}
 void main (void)
 {
     OSCCONbits.IRCF0 = 0;  // Set internal clock to 4 MHz next 3 instr.
@@ -153,38 +213,10 @@ void main (void)
     y1 = 10000 - x1;
     y2 = 10000 - x2;
   
-
-    TRISB = 0;
-            
-    TRISBbits.TRISB2 = 1;   //R encoder input
-    TRISBbits.TRISB1 = 1;   //L encoder input
-    TRISBbits.TRISB0 = 1;
-    
-    TRISCbits.TRISC1 = 0;	// R signal out
-    TRISCbits.TRISC2 = 0;   // L signal out
-    TRISAbits.TRISA4 = 1;
    
-    
-    RCONbits.IPEN   = 1; // enable interrupt priority
-    INTCONbits.GIE  = 1; // enable interrupts
-    INTCONbits.PEIE = 1; // enable peripheral interrupts.
-    INTCON3bits.INT1IE = 1;
-    INTCON3bits.INT2IE = 1;
-    INTCON3bits.INT1P = 0;
-    INTCON3bits.INT2P = 0;
-    INTCON2bits.INTEDG1 = 0;
-    INTCON2bits.INTEDG2 = 0;
-    
-    INTCONbits.TMR0IE = 1;
-    INTCON2bits.TMR0IP = 0;
-    
-    
-T0CONbits.T08BIT = 0;
-T0CONbits.T0CS = 0;
-T0CONbits.PSA = 1;
-TMR0H = 0x3C;
-TMR0L = 0xB0;
-T0CONbits.TMR0ON = 1;
+    inoutinit();
+    intinit();
+    timerinit();
 
     while(1)
     { 
@@ -193,40 +225,8 @@ T0CONbits.TMR0ON = 1;
     
     
     INTCONbits.GIE = 0;
-    PORTCbits.RC2 = 1;
-    Delay10TCYx(x1);
-    PORTCbits.RC2 = 0;
-    Delay10TCYx(y1);
-    PORTCbits.RC1 = 1;
-    Delay10TCYx(x2);
-    PORTCbits.RC1 = 0;
-    Delay10TCYx(y2);
-    
-     x2 = slavePower;
-     errorprev = error;
-     error = encodercountl - encodercountr;
-     errorfreq = freqlavg - freqravg;
-     
-    
-     derror = error - errorprev;
- 
-    slavePower += ((errorfreq * kp) + (derror * kd) + (ierror * ki));
-    
-    if( slavePower >= 156)
-    {
-        slavePower = 150;
-    }
-    if(slavePower <= 103)
-    {
-        slavePower = 150;
-    }
-    
-    ierror = ierror + error;
-    
-   
-    encodercountl = 0;
-    encodercountr = 0;
-    
+    speedupdate();
+    calcerror();
     INTCONbits.GIE = 1;
      
     Delay100TCYx(125);
@@ -238,7 +238,7 @@ T0CONbits.TMR0ON = 1;
              x2 = x2 + 1;
              Delay10KTCYx(5); 
          }
-         if(PORTBbits.RB0 == 0)
+    if(PORTBbits.RB0 == 0)
          {
              x1 = x1 - 1;
              x2 = x2 - 1;
